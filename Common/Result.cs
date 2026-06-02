093dc4102cc8ab0d0ee7a252e9c97c0520aa039f
Hernan Riano <hriano2@gmail.com>
2026-06-02 11:06:03 +1000

    feat: add  Result<T> class

--- Common/Result.cs
+++ Common/Result.cs
@@ -6,7 +6,48 @@ using System.Threading.Tasks;
 
 namespace TasksFlowConsole.Common
 {
-    internal class Result
+    public class Result
     {
+        public bool Success { get; }
+        public string Error { get; }
+
+        public bool IsFailure => !Success;
+
+        protected Result(bool success, string error)
+        {
+            Success = success;
+            Error = error;
+        }
+        
+        public static Result Ok()
+        {
+            return new Result(true, string.Empty);
+        }
+
+        public static Result Fail (string error)
+        {
+            return new Result(false, error);
+        }
     }
+
+    public class Result<T> : Result
+    {
+        public T? Value { get; }
+
+        private Result(bool success, T? value, string error) : base(success, error)
+        {
+            Value = value;
+        }
+
+        public static new Result<T> Ok(T value)
+        {
+            return new Result<T>(true, value, string.Empty);
+        }
+
+        public static new Result<T> Fail(string error)
+        {
+            return new Result<T>(false, default, error);
+        }
+    }
+
 }
