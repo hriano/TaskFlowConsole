717d49af968c0f7a5870bfab9f1b09425e27e32d
Hernan Riano <hriano2@gmail.com>
2026-06-05 09:45:50 +1000

    refactor : Protected UserTask instantiation throug Factory Method

--- Models/UserTask.cs
+++ Models/UserTask.cs
@@ -9,12 +9,28 @@ namespace TasksFlowConsole.Models
 {
    public class UserTask
     {
-        //private bool _isCompleted = false;
-        //public bool IsCompleted => _isCompleted;
-        //private string _title;
-        public string Title { get; set; }
-        public int Id { get; set; }
-        public Enums.TaskStatus Status { get;  set; }
+        
+        public string Title { get; }
+        public int Id { get; }
+        public Enums.TaskStatus Status { get; private set; }
+
+        private UserTask(int id, string title)
+        {
+            Id = id;
+            Title = title;
+            Status = Enums.TaskStatus.Pending;
+        }
+
+        public static UserTask CreateTask(int id, string title)
+        {
+            if (id <= 0)
+              throw new ArgumentException("Invalid Id");
+            
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("Invalid title");
+
+            return new UserTask(id, title);
+        }
 
 
 
