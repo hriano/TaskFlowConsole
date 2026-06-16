d6300ea4ce69103e44f86aab3a2e69ed92339142
Hernan Riano <hriano2@gmail.com>
2026-06-17 08:47:36 +1000

    refactor: Apply Builder by Menus

---
+++ Presentation/Menus/MenuBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksFlowConsole.Presentation.Menus
+{
+    public class MenuBuilder
+    {
+        private readonly List<MenuOption> Options = new();
+
+        public MenuBuilder AddOption(string description, Action execute)
+        {
+            int optionNumber = Options.Count + 1;
+
+            Options.Add(new MenuOption(optionNumber, description, execute));
+            
+            return this;
+
+        }
+
+        public MenuBuilder AddExitOption(string description)
+        {
+            int optionNumber = Options.Count + 1;
+            
+            Options.Add(new MenuOption(optionNumber, description,null, true));
+
+            return this;
+        }
+
+        public List<MenuOption> Build()
+        {
+            if (!Options.Any())
+                throw new InvalidOperationException("Menu requires at least one option.");
+
+            return Options;
+        }
+    }
+}
---
+++ Presentation/Menus/MenuOption.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksFlowConsole.Presentation.Menus
+{
+    public class MenuOption
+    {
+        public int Option { get; }
+        public string Description { get; }
+        public Action Execute { get; }
+        public bool ExitMenu { get; }
+
+        public MenuOption(int option, string description, Action? execute, bool exitMenu = false)
+        {
+            Option = option;
+            Description = description;
+            Execute = execute;
+            ExitMenu = exitMenu;
+        }
+    }
+}
